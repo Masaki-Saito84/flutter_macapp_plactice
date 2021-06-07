37df9ae3f7725c0ed8ba0378e89f3bcf1cdde905
Masaki-Saito84 <saito@hyge.co.jp>
2021-06-07 18:57:04 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/mac_os_webview/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mac_os_webview
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_macos_webview
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterMacOSWebViewPlugin.register(with: registry.registrar(forPlugin: "FlutterMacOSWebViewPlugin"))
+}
