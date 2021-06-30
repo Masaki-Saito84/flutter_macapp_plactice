4dd2754e77d3306323521b6e177fa8611172dda8
Masaki-Saito84 <saito@hyge.co.jp>
2021-06-30 19:23:09 +0900

    :wrench: 表示するモーダルサイズをappのウィンドウサイズと同じサイズにする。

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,6 @@ class App extends StatelessWidget {
     await webview.open(
       url: 'https://google.com',
       presentationStyle: presentationStyle,
-      // size: Size(400.0, 400.0),
       userAgent:
           'Mozilla/5.0 (iPhone; CPU iPhone OS 14_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1',
     );
