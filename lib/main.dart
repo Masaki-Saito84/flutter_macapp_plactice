1e7cf692a4a01f5ac749111bf5ad1e0f0b982177
Masaki-Saito84 <saito@hyge.co.jp>
2021-07-08 11:00:56 +0900

    :wrench: webviewの起動関数をAppクラスの外に出す。引数にURL追加、変更可能にする。ステート保持のため、app homeのwidgetを一つ子階層に変更。

--- lib/main.dart
+++ lib/main.dart
@@ -4,27 +4,40 @@ import 'package:flutter_macos_webview/flutter_macos_webview.dart';
 void main() => runApp(App());
 
 class App extends StatelessWidget {
-  Future<void> _onOpenPressed(PresentationStyle presentationStyle) async {
-    final webview = FlutterMacOSWebView(
-      onOpen: () => print('Opened'),
-      onClose: () => print('Closed'),
-      onPageStarted: (url) => print('Page started: $url'),
-      onPageFinished: (url) => print('Page finished: $url'),
-      onWebResourceError: (err) {
-        print(
-          'Error: ${err.errorCode}, ${err.errorType}, ${err.domain}, ${err.description}',
-        );
-      },
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+      debugShowCheckedModeBanner: false,
+      home: LauncherHome(),
     );
-
-    await webview.open(
-      url: 'https://hyge.my.salesforce.com/?ec=302&startURL=%2Fvisualforce%2Fsession%3Furl%3Dhttps%253A%252F%252Fhyge.lightning.force.com%252Flightning%252Fpage%252Fhome',
-      presentationStyle: presentationStyle,
-      userAgent:
-          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0 Safari/605.1.15',
-    );
-
   }
+}
+
+class LauncherHome extends StatefulWidget {
+  @override
+  _LauncherHomeState createState() => _LauncherHomeState();
+}
+
+Future<void> _onOpenPressed(PresentationStyle presentationStyle, String targetUrl) async {
+  final webview = FlutterMacOSWebView(
+    onOpen: () => print('Opened'),
+    onClose: () => print('Closed'),
+    onPageStarted: (url) => print('Page started: $url'),
+    onPageFinished: (url) => print('Page finished: $url'),
+    onWebResourceError: (err) {
+      print(
+        'Error: ${err.errorCode}, ${err.errorType}, ${err.domain}, ${err.description}',
+      );
+    },
+  );
+
+  await webview.open(
+    url: targetUrl,
+    presentationStyle: presentationStyle,
+    userAgent:
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0 Safari/605.1.15',
+  );
+}
 
   @override
   Widget build(BuildContext context) {
